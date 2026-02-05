81f16e5a3ac87a2036c9b24050beaf3f7b40b9cd
retrobricker <smblolwut@gmail.com>
2026-02-05 02:08:50 -0600

    Removed \app\login and \app\signup routes instead for \app\auth\login and \app\auth\signup routes, correctly utilizing the paths intended

--- app/login/page.tsx
+++
@@ -1,9 +0,0 @@
-import AuthForm from "@/components/features/auth/AuthForm";
-
-export default function LoginPage() {
-  return (
-    <main className="flex min-h-screen items-center justify-center">
-      <AuthForm title="Login" submitLabel="Log in" />
-    </main>
-  );
-}
