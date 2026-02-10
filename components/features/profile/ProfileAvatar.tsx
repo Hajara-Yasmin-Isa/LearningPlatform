b280785d0822234ae23ee1607692a3fe75b61ff1
retrobricker <smblolwut@gmail.com>
2026-02-10 06:06:50 -0600

    Prototype ProfileAvatar component kind of done-- button needs styling

--- components/features/profile/ProfileAvatar.tsx
+++ components/features/profile/ProfileAvatar.tsx
@@ -5,9 +5,25 @@ import { useState } from "react";
 export default function ProfileAvatar({ image, size }: ProfileAvatarProps) {
   const avatarSize = size ?? 96;
   const [avatarSrc, setAvatarSrc] = useState(image ?? "/default.jpeg");
-}
 
-return {
-    
+
+return (
+  <div>
+    {/* Avatar */}
+    <img
+        src={avatarSrc}
+        alt="Profile avatar"
+        style={{ width: avatarSize, height: avatarSize }}
+        className="rounded-full border"
+    />
+
+
+    {/* Action */}
+    <button type="button">
+      Change avatar
+    </button>
+  </div>
+);
+
 }
 
