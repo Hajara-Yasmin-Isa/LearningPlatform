d342094578c95b48f5aa5d1d48cae049ccbe15cc
Shujath Muqeem <smblolwutendstop@gmail.com>
2026-03-06 02:41:41 -0600

    Created some imports and stuff to start

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import { useState } from "react";
+import DashboardNav from "@/components/features/dashboard/DashboardNav";
+import StudentDashboard from "@/components/features/dashboard/StudentDashboard";
+
+type Role = "Student" | "Instructor";
+
+
+
