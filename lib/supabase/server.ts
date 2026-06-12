42ebf481a465e353cfc9358dc14744ef003469d6
isa <elhamisa04@gmail.com>
2026-06-12 14:56:52 -0500

    fixed the server.ts with ssr cookie based client instend of persistsessions: false because the API is outdated for the current supabase version. functionality is the same but the only differnt is usage of getAll and setAll instead of get and set, respectively.

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,21 +1,38 @@
-import {createClient} from '@supabase/supabase-js'
+//import {createClient} from '@supabase/supabase-js'
 
-export function createServerClient() {
-    const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-    const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+import { createServerClient as createSSRClient } from "@supabase/ssr"
+import { cookies} from 'next/headers'
+
+
+export async function createServerClient() {
+    const cookieStore = await cookies()
     
     // this basicallly validate that environment variables are set
-    if (!supabaseUrl || !supabaseAnonKey) {
-        throw new Error(
-            'Missing Supabase environment variables. Check your .env.local file.'
-        )
-    }
+    // if (!supabaseUrl || !supabaseAnonKey) {
+    //     throw new Error(
+    //         'Missing Supabase environment variables. Check your .env.local file.'
+    //     )
+    // }
 
-    return createClient(supabaseUrl, supabaseAnonKey, {
-        auth: {
-            persistSession: false,
-            autoRefreshToken: false,
-            detectSessionInUrl: false,
+
+
+    return createSSRClient(
+        process.env.NEXT_PUBLIC_SUPABASE_URL!,
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+        {
+            cookies: {
+                getAll() {
+                    return cookieStore.getAll()
+                },
+                setAll(cookiesToSet: {name: string; value: string; options?: any}[]) {
+                    try {
+                        cookiesToSet.forEach(({name, value, options }) =>
+                            cookieStore.set(name, value, options))
+                    } catch {
+
+                    }
+                }
+            },
         }
-    })
+    )
 }
