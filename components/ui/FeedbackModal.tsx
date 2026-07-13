c6a1f0a1e228372eb1681ae93dd947bc8334cbaa
Shujath Muqeem <smblolwutendstop@gmail.com>
2026-07-13 03:15:50 -0500

    Creating FeedbackModal structure

--- components/ui/FeedbackModal.tsx
+++ components/ui/FeedbackModal.tsx
@@ -1,3 +1,10 @@
 'use client'
 
 //Waiting for backend task 3 to be available on dev to implement
+export default function FeedbackModal() {
+    return (
+        <select>
+            
+        </select>
+    )
+}
