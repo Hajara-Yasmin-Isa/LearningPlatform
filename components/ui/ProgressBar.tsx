af6e9a6da5074837c778af86b38a48507187dd82
Shujath Muqeem <smblolwutendstop@gmail.com>
2026-07-20 11:59:53 -0500

    Calculating percentage of total

--- components/ui/ProgressBar.tsx
+++ components/ui/ProgressBar.tsx
@@ -5,5 +5,9 @@ interface ProgressBarProps {
 }
 
 export default function ProgressBar({ completed, total, className }: ProgressBarProps) {
-    if (total === 0) {}
+
+    const percentage = total === 0 ? 0 : Math.round(completed/total)
+    
+    if (total === 0) { return <p className="text-gray-500 mt-8">Babu sashe tukuna</p>} 
+    
 }
